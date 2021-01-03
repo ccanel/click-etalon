5adefb8783492c95f8c4a010a67af216cebf45c9
Shawn Chen <sschenshawn@gmail.com>
2021-01-03 14:28:30 -0800

    Improve log printing in RunSchedule component. 1. Introduce a timer-based mechanism to run in parallel with the counter-based mechanism. Logs will be printed either every 100 configs or every 10 seconds, whichever occurs first. 2. Expose print_every_n_config and print_every_n_sec as configurables via write handlers.

--- elements/standard/run_schedule.hh
+++ elements/standard/run_schedule.hh
@@ -4,6 +4,7 @@
 #include <click/element.hh>
 #include <click/timer.hh>
 #include <pthread.h>
+#include <time.h>
 CLICK_DECLS
 
 /*
@@ -50,6 +51,8 @@ class RunSchedule : public Element {
     static String get_marking_thresh(Element*, void *);
     static Vector<String> split(const String&, char);
     int execute_schedule(ErrorHandler *);
+    static int print_n_config_handler(const String&, Element*, void*, ErrorHandler*);
+    static int print_n_sec_handler(const String&, Element*, void*, ErrorHandler*);
 
     bool new_sched;
     Task _task;
@@ -64,8 +67,12 @@ class RunSchedule : public Element {
     HandlerCall **_packet_pull_switch;
     HandlerCall *_ece_map;
     HandlerCall *_log_config;
-    HandlerCall * _icmp_tdn_handler;
     int _print;
+    clock_t _last_print_time;
+    // Print periodic schedule logs every N configs.
+    int _print_every_n_config;
+    // Print periodic schedule logs every N seconds.
+    double _print_every_n_sec;
     int _in_advance;
     struct timespec _start_time;
     long long _next_time;
